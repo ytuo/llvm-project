251b3a515be6768253e3aff8d20887924659722c
Yannan <ytuo@berkeley.edu>
2020-06-12 12:42:32 -0700

    nonpod classdef cleanup

--- clang-tools-extra/clang-tidy/bsl/NonPodClassdefCheck.cpp
+++ clang-tools-extra/clang-tidy/bsl/NonPodClassdefCheck.cpp
@@ -16,98 +16,48 @@ namespace clang {
 namespace tidy {
 namespace bsl {
 
-// AST_MATCHER(CXXRecordDecl, hasVirtualBases) {
-//   CXXRecordDecl *Def = Node.getDefinition();
-//   return Def->getNumVBases() > 0;
-// }
-
 void NonPodClassdefCheck::registerMatchers(MatchFinder *Finder) {
-  // Finder->addMatcher(cxxRecordDecl(has(accessSpecDecl(anyOf(isPublic(), isProtected())))).bind("private"), this);  // cxxRecordDecl
-  // // Finder->addMatcher(fieldDecl(unless(isPrivate())).bind("private"), this);
+  Finder->addMatcher(cxxRecordDecl(has(accessSpecDecl(anyOf(isPublic(), isProtected())))).bind("private"), this); 
+  // Finder->addMatcher(fieldDecl(unless(isPrivate())).bind("private"), this);
 
-  // Finder->addMatcher(cxxRecordDecl(hasDefinition(), unless(isClass())).bind("class"), this);
+  Finder->addMatcher(cxxRecordDecl(hasDefinition(), unless(isClass())).bind("class"), this);
 
-  Finder->addMatcher(cxxRecordDecl(has(
-                                  accessSpecDecl(anyOf(isPrivate(), isProtected()))), 
-                                  isStruct(),
-                                  hasAnyBase()
-                    ).bind("struct"), this);  // cxxRecordDecl
-
-  // Finder->addMatcher(cxxRecordDecl(hasDefinition(), isStruct(), hasAnyBase(), has(fieldDecl(unless(isPublic())))).bind("class"), this);
-
-  // check parent class is not struct, instead of check itself not a parent,  isDerivedFrom
-  // isDefaulted
-  // Finder->addMatcher(tagDecl(isStruct(), ));
-  // fieldDecl(isPublic())
-  // hasAnyBase()
-  // check other classes and struct: check base not isStruct()
-  
-    // Finder->addMatcher(cxxBaseSpecifier(isVirtual()))
-  // trivial
-  // Finder->addMatcher(decl(anyOf(cxxRecordDecl(hasDefinition(), hasVirtualBases()),
-  //           cxxMethodDecl(isVirtual())
-  //           )).bind("class"), this);
-
-  // cxxConstructorDecl(anyOf(isMoveConstructor(), isCopyConstructor()))
-  // unless(cxxConstructorDecl(isDefaultConstructor()), cxxDestructorDecl(isDefaulted()))  // no ConstructorDecl
-
-  // // standard layout
-  // functionDecl(isStaticStorageClass()), unless(standard layout??)
-  // varDecl(isStaticStorageClass()), unless(standard layout??)
-
-  /* 1. Check if not trivially copyable: has virtual anything, has non-default constructor
-     2. Check not standard-layout: has static data, not "same access control," at most one base class subobject
-     3. If not either one (ie, not POD):
-         1. match if not a class (ie, if union, struct, typedef, etc)
-         2. match if memberdata not isPrivate, ie isProtected, isPublic, or isVirtual
-     4. If POD, do nothing
-     5. but if POD struct, check 1-4
-  */
-  // don't need to check for static
-  // hasInClassInitializer, isClass, but vars not isPrivate, ie isProtected, isPublic, or isVirtual
-
-  // or isStruct that does not satisfy (1)-(4), ie has non-public data, has funcions, is a base class, or inherits from something
-
-  // but if POD class type not struct, ignore (class with no constructor?)
 }
 
 void NonPodClassdefCheck::check(const MatchFinder::MatchResult &Result) {
   // member data
-  // auto Var = Result.Nodes.getNodeAs<CXXRecordDecl>("private");
-  // if (Var) {
-  //     auto Loc = Var->getLocation();
-  //   if (Loc.isInvalid() || Loc.isMacroID())
-  //     return;
+  auto Var = Result.Nodes.getNodeAs<CXXRecordDecl>("private");
+  if (Var) {
+    auto Loc = Var->getLocation();
+    if (Loc.isInvalid() || Loc.isMacroID())
+      return;
 
-  //   auto Mgr = Result.SourceManager;
-  //   if (Mgr->getFileID(Loc) != Mgr->getMainFileID())
-  //     return;
+    auto Mgr = Result.SourceManager;
+    if (Mgr->getFileID(Loc) != Mgr->getMainFileID())
+      return;
 
-  //   if (Var->isPOD())
-  //     return;
-  //   else
-  //     diag(Loc, "non-POD class types should have private member data");
-  // }
+    if (Var->isPOD())
+      return;
+    else
+      diag(Loc, "non-POD class types should have private member data");
+  }
 
-  // // class type
-  // auto NonPodClass = Result.Nodes.getNodeAs<CXXRecordDecl>("class");
+  // class non-POD type
+  auto NonPodClass = Result.Nodes.getNodeAs<CXXRecordDecl>("class");
 
-  // if (NonPodClass) {
-  //   auto PodLoc = NonPodClass->getLocation();
+  if (NonPodClass) {
+    auto PodLoc = NonPodClass->getLocation();
+    if (PodLoc.isInvalid() || PodLoc.isMacroID())
+      return;
 
-  //   if (NonPodClass->isPOD())
-  //     return;
-  //   else
-  //     diag(PodLoc, "non-POD type should be defined as a class");
-  // }
+    auto Mgr = Result.SourceManager;
+    if (Mgr->getFileID(PodLoc) != Mgr->getMainFileID())
+      return;
 
-  // struct
-  auto StructDef = Result.Nodes.getNodeAs<CXXRecordDecl>("struct");
-
-  if (StructDef) {
-    auto StructLoc = StructDef->getLocation();
-
-    diag(StructLoc, "struct");
+    if (NonPodClass->isPOD())
+      return;
+    else
+      diag(PodLoc, "non-POD type should be defined as a class");
   }
 
 }
--- clang-tools-extra/test/clang-tidy/checkers/bsl-non-pod-classdef.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bsl-non-pod-classdef.cpp
@@ -1,93 +1,34 @@
 // RUN: %check_clang_tidy %s bsl-non-pod-classdef %t
 
-class N { // neither trivial nor standard-
-   private: 
-    int i;
-   public: 
-    int j;
-    // virtual ~N();
+class NonPod{ // not trivial nor standard-layout
+    int a;
+    int b;
+    virtual void foo();
 };
 
-struct T { // trivial but not standard-layout
+class Trivial { // trivial only
 public:
-    int i;
-    int j;
-};
-
-struct SL: T { // standard-layout but not trivial
+    int a;
 private:
-
-    int i;
-    int j;
-    // ~SL();
+    int b;
 };
 
-N n;
-T t;
-SL sl;
+class StandardL { // standard-layout only
+    int a;
+    int b;
+public:
+    ~StandardL();
+};
 
-// class NonPOD1 {
+class Pod {   // trivial & standard-layout, ie POD
+    // default private
+    int a;
+    int b;
+};
 
-// };
-
-// class POD1 {
-
-// };
-
-// /// @brief POD type
-// struct foo final
-// { 
-//     bool data;
-// };
-// /// @brief non-POD type
-// class non_pod_foo final
-// { 
-// public:
-//     bool data{true};
-// };
-// /// @brief non-compliant - defines global and initializes at runtime
-// foo g_bar1{};
-// /// @brief compliant - defines global and initializes at compile time
-// foo g_bar2;
-/// @brief compliant - defines global and initializes at compile time 
-///    using constinit. Note that because this is a non-POD type, use 
-///    of the {} is not needed, but is fine since this was paired with 
-///    constinit. 
-// constinit non_pod_foo g_bar3{};
-// /// @brief non-compliant - defines static and initializes at runtime
-// /// @return a reference to foo
-// [[nodiscard]] foo const &
-// get_bar1() noexcept 
-// {
-//     static foo s_bar1{};
-//     return s_bar1;
-// }
-// /// @brief compliant - defines static local and initializes at compile time
-// /// @return a reference to foo
-// [[nodiscard]] foo const &
-// get_bar2() noexcept 
-// {
-//     static foo s_bar2;
-//     return s_bar2;
-// }
-// /// @brief compliant - defines static local and initializes at compile time 
-// ///    using constinit. Note that because this is a non-POD type, use 
-// ///    of the {} is not needed, but is fine since this was paired with 
-// ///    constinit. 
-// /// @return a reference to non_pod_foo
-// [[nodiscard]] non_pod_foo const &
-// get_bar3() noexcept 
-// {
-//     constinit static non_pod_foo s_bar3{};
-//     return s_bar3;
-// }
-// /// @brief a function that does something
-// constexpr void
-// do_something() noexcept 
-// {
-//     foo bar1; /// non-compliant - foo is POD but not initialized
-//     foo bar2{}; /// compliant - foo is POD and is explicitly initialized
-//     non_pod_foo bar2; /// compliant - non_pod_foo is a non-POD.
-//     non_pod_foo bar3{}; /// compliant - non_pod_foo is a non-POD.
-//     return s_bar2;
-// }
+struct NonPodStruct { //  should be defined as class
+public:
+    int a;
+private:
+    int b;
+};
