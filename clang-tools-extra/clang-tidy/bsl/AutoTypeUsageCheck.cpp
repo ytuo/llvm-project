bf4404ede168ec7368d0a9fcd8d5a61782d921bc
Yannan <ytuo@berkeley.edu>
2020-07-01 11:29:41 -0700

    funcdecl ip

--- clang-tools-extra/clang-tidy/bsl/AutoTypeUsageCheck.cpp
+++ clang-tools-extra/clang-tidy/bsl/AutoTypeUsageCheck.cpp
@@ -16,6 +16,25 @@ namespace clang {
 namespace tidy {
 namespace bsl {
 
+// AST_MATCHER(CXXMethodDecl, isTemplate) {
+//   return Node.getParent() == FunctionDecl::TK_FunctionTemplate;
+// }
+
+AST_MATCHER(ValueDecl, isFundamentalType) {
+  return Node.getType().getTypePtr()->isFundamentalType();
+}
+
+
+AST_MATCHER(FunctionDecl, isMethod) {   // works
+  return Node.getDeclKind() == Decl::CXXMethod;
+}
+
+AST_MATCHER(FunctionDecl, isReturnAuto) {   
+  // return Node.getReturnType().getTypePtr()->hasAutoForTrailingReturnType();
+  return Node.getReturnType().getTypePtr()->getContainedAutoType();
+}
+
+
 void AutoTypeUsageCheck::registerMatchers(MatchFinder *Finder) {
   // var: is return value and has type of function
   // 
@@ -23,10 +42,32 @@ void AutoTypeUsageCheck::registerMatchers(MatchFinder *Finder) {
   // (2) cxxCtorInitializer(unless()) ??? --> binaryoperator -> hasType RHS = not Type isFundamentalType() 
   // (3) lambdaExpr parmVarDecl(hasType(autoType()))
   // (4) functionDecl(hasTrailingReturn()), hasType(autoType())
-  Finder->addMatcher(expr(hasType(autoType())).bind("x"), this);
-  Finder->addMatcher(valueDecl(hasType(autoType())).bind("decl"), this);
-  Finder->addMatcher(classTemplateDecl(hasType(autoType())).bind("temp"), this);  //  functionTemplateDecl
+  // Finder->addMatcher(expr(hasType(autoType())).bind("x"), this);   // not needed
+  // Finder->addMatcher(valueDecl(hasType(autoType())    // vardecl?
+  //                   // unless(valueDecl(ignoringImpCasts(hasType(autoType())))
+  //                   ).bind("decl"), this); 
 
+  // Finder->addMatcher(valueDecl(hasType(autoType()), isFundamentalType(),    // vardecl?
+  //                   unless(has(callExpr()))     // only has? what if 1 + f()
+  //                   ).bind("decl"), this);  // ok?
+
+  // Finder->addMatcher(functionDecl(hasTrailingReturn(), anyOf(hasParent(functionTemplateDecl()), hasParent(classTemplateDecl()))).bind("trail"), this);
+  // Finder->addMatcher(functionDecl(isReturnAuto()        // returns(unless(autoType())) // unless(    // isDefinition()
+  //         // anyOf(
+  //         //   isMethod(),
+  //           // allOf(hasTrailingReturn(), 
+  //           //   anyOf(hasParent(functionTemplateDecl()), hasParent(classTemplateDecl())))
+  //         // )
+  //         ).bind("trail"), this);
+
+  // Finder->addMatcher(cxxMethodDecl(unless(hasTrailingReturn())).bind("trail"), this); // parital ok
+    Finder->addMatcher(cxxMethodDecl().bind("trail"), this);  // ok ish
+
+  // template specialization too?
+
+   // inder->addMatcher(classTemplateDecl(has(returnStmt(hasReturnValue(hasType(autoType()))))).bind("temp"), this);  //  functionTemplateDecl
+
+  // friend auto?
 }
 
 void AutoTypeUsageCheck::check(const MatchFinder::MatchResult &Result) {
@@ -40,9 +81,32 @@ void AutoTypeUsageCheck::check(const MatchFinder::MatchResult &Result) {
   const auto *MatchedDecl = Result.Nodes.getNodeAs<VarDecl>("decl");
   if (MatchedDecl)
     diag(MatchedDecl->getLocation(), "decl is insufficiently awesome");
-  const auto *MatchedTempDecl = Result.Nodes.getNodeAs<ClassTemplateDecl>("decl");
-  if (MatchedTempDecl)
-    diag(MatchedTempDecl->getLocation(), "temp decl is insufficiently awesome");
+
+  // const auto *MatchedTempDecl = Result.Nodes.getNodeAs<FunctionDecl>("trail");
+  // if (MatchedTempDecl)
+  //   diag(MatchedTempDecl->getLocation(), "temp decl is insufficiently awesome");
+
+  const auto *MatchedClassDecl = Result.Nodes.getNodeAs<CXXMethodDecl>("trail");
+  
+  if (MatchedClassDecl) {
+    diag(MatchedClassDecl->getLocation(), "temp decl is insufficiently derp ");
+    const auto *MatchedClassDecl2 = MatchedClassDecl->getParent();
+    
+    if (MatchedClassDecl2->getDescribedClassTemplate()->isThisDeclarationADefinition()) {   // does this do anything
+      diag(MatchedClassDecl->getLocation(), "bleh ");
+    }
+    if (MatchedClassDecl->getTemplatedKind() == FunctionDecl::TK_NonTemplate)
+      diag(MatchedClassDecl->getLocation(), "temp decl is insufficiently 1 ");
+    if (MatchedClassDecl->getTemplatedKind() == FunctionDecl::TK_MemberSpecialization)
+      diag(MatchedClassDecl->getLocation(), "temp decl is insufficiently 2 ");
+    if (MatchedClassDecl->getTemplatedKind() == FunctionDecl::TK_FunctionTemplateSpecialization)
+      diag(MatchedClassDecl->getLocation(), "temp decl is insufficiently 3 ");
+    if (MatchedClassDecl->getTemplatedKind() == FunctionDecl::TK_FunctionTemplate)
+      diag(MatchedClassDecl->getLocation(), "temp decl is insufficiently 4 ");
+    if (MatchedClassDecl->getTemplatedKind() == FunctionDecl::TK_DependentFunctionTemplateSpecialization)
+      diag(MatchedClassDecl->getLocation(), "temp decl is insufficiently 5 ");
+  }
+    
 }
 
 } // namespace bsl
--- clang-tools-extra/test/clang-tidy/checkers/bsl-auto-type-usage.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bsl-auto-type-usage.cpp
@@ -4,39 +4,40 @@
 // https://isocpp.org/wiki/faq/cpp14-language
 
 
-#include <cstdint>
-#include <vector>
+// #include <cstdint>
+// #include <vector>
 
-class A
-{
-};
+// class A
+// {
+// };
 
-void f1() noexcept
-{
-	auto x1 = 5; // Non-compliant - initializer is of fundamental type
-	auto x2 = 0.3F; // Non-compliant - initializer is of fundamental type
-	auto x3 = {8}; // Non-compliant - initializer is of fundamental type
+// void f1() noexcept
+// {
+// 	auto x1 = 5; // Non-compliant - initializer is of fundamental type
+// 	auto x2 = 0.3F; // Non-compliant - initializer is of fundamental type
+// 	// auto x3 = {8}; // Non-compliant - initializer is of fundamental type
 
-	std::vector<std::int32_t> v;
-	auto x4 = v.size(); // Compliant with case (1) - x4 is of size_t type that
-	// is returned from v.size() method
-	auto a = A{}; // Compliant with case (2)
+// 	std::vector<std::int32_t> v;
+// 	auto x4 = v.size(); // Compliant with case (1) - x4 is of size_t type that is returned from v.size() method
+// 	auto a = A{}; // Compliant with case (2)
 
-	auto lambda1 = []() -> std::uint16_t {
-		return 5U;
-	}; // Compliant with case (2) - lambda1 is of non-fundamental lambda
-	// expression type
-	auto x5 = lambda1(); // Compliant with case (1) - x5 is of
-	// std::uint16_t type
-}
+// 	auto lambda1 = []() -> std::uint16_t {
+// 		return 5U;
+// 	}; // Compliant with case (2) - lambda1 is of non-fundamental lambda expression type
+// 	auto x5 = lambda1(); // Compliant with case (1) - x5 is of std::uint16_t type
+// }
 
-void f2() noexcept
-{
-	auto lambda1 = [](auto x, auto y) -> decltype(x + y) {
-		return (x + y);
-	}; // Compliant with cases (2) and (3)
-	auto y1 = lambda1(5.0, 10); // Compliant with case (1)
-}
+// void f2() noexcept
+// {
+// 	auto lambda1 = [](auto x, auto y) -> decltype(x + y) {
+// 		return (x + y);
+// 	}; // Compliant with cases (2) and (3)
+// 	auto y1 = lambda1(5.0, 10); // Compliant with case (1)
+// }
+
+// // void f4(auto x) 	// not allowed
+// // {
+// // }
 
 template <typename T, typename U>
 auto f3(T t, U u) noexcept -> decltype(t + u) // Compliant with case (4)
@@ -56,3 +57,33 @@ auto B<T>::fn(T t) -> T // Compliant with case (4)
 {
 	return t;
 }
+
+// template <typename U>
+// auto B<U>::fn(U t) -> U // Compliant with case (4)
+// {
+// 	return t;
+// }
+
+
+// class C
+// {
+// 	auto fn(int x, int y) -> int;  // function return type = auto --> must have template decl?
+// 	// {
+// 	// 	return x;
+// 	// }
+// };
+auto fn1(int x, int y) -> int// function return type = auto --> must have template decl?
+{
+	return x + y;
+}
+
+
+// auto fn(int x, int y) // function return type = auto --> must have template decl?
+// {
+// 	return x + y;
+// }
+
+// auto fn(int x, int y, int z);
+
+template <typename T>
+auto fn(T x, T y) -> T; // function return type = auto --> must have template decl?
