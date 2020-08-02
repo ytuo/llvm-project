5786513eb40bae45bccd698233021126d28cb1e8
Connor Davis <connojd@protonmail.com>
2020-08-02 00:22:14 -0600

    bsl-class-member-init: check templates; ignore defaulted
    Make sure the matchers take into account instantiated
    constructors vs non-instantiated constructors. The
    CXXConstructorDecl::isDelegatingConstructor member function is only
    valid for instantiated decls.
    
    Also ignore explicitly defaulted constructors, as they dont cause
    confusion among what is getting initialized.

--- clang-tools-extra/test/clang-tidy/checkers/bsl-class-member-init.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bsl-class-member-init.cpp
@@ -66,3 +66,52 @@ F::F(int arg0, int arg1)
     f0 = arg0;
     f1 = arg1;
 }
+
+template<typename T>
+class G final {
+    int m_x;
+    long m_val;
+
+public:
+
+    constexpr G(const int x, T const &val) noexcept
+        : m_x{x}, m_val{val}
+    {}
+
+    constexpr G(const int *i, T const &val) noexcept
+        : G{*i, val}
+    {}
+
+    constexpr G() noexcept
+    {}
+
+    constexpr G(int x) noexcept
+        : m_x{x}
+    {}
+};
+
+static int foo()
+{
+    int i = 0;
+    long val = 1;
+
+    G<long> m0;
+// CHECK-MESSAGES: :[[@LINE-14]]:5: warning: must use either in-class initializers for all fields or constructor initializers for all fields [bsl-class-member-init]
+
+    G<long> m1{i};
+// CHECK-MESSAGES: :[[@LINE-14]]:5: warning: member initializer list does not initialize each field [bsl-class-member-init]
+
+    G<long> m2{&i, val}; // delegating constructor is fine
+    G<long> m3{i, val};
+
+    return i;
+}
+
+template<typename T>
+class H final {
+    int m_x;
+
+public:
+    constexpr H() noexcept = default;
+};
+
