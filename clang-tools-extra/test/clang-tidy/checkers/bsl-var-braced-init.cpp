f66bf9a61c6178db0aa9a866d22fcddb2f9110ce
Connor Davis <connojd@protonmail.com>
2020-08-04 19:52:12 -0600

    bsl-var-braced-init: Allow {} with auto, fix ranged-for bugs
    - Bump the supported language version to CPlusPlus17
    - Ensure that auto-declared variables use braced-init-list
    - Make exceptions for variables declared in ranged for-loops

--- clang-tools-extra/test/clang-tidy/checkers/bsl-var-braced-init.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bsl-var-braced-init.cpp
@@ -1,4 +1,4 @@
-// RUN: %check_clang_tidy %s bsl-var-braced-init %t
+// RUN: %check_clang_tidy -std=c++17-or-later %s bsl-var-braced-init %t
 
 namespace std {
 template<typename T>
@@ -9,10 +9,10 @@ template<typename T>
 void f(T t)
 {
     int x = 4;
-    // CHECK-MESSAGES: :[[@LINE-1]]:9: warning: variable is not initialized via direct list initialization [bsl-var-braced-init]
+    // CHECK-MESSAGES: :[[@LINE-1]]:9: warning: variable 'x' is not initialized via direct list initialization [bsl-var-braced-init]
 
     T t0(x);
-    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: variable is not initialized via direct list initialization [bsl-var-braced-init]
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: variable 't0' is not initialized via direct list initialization [bsl-var-braced-init]
 
     T t1{4};
 }
@@ -22,19 +22,21 @@ int global;
 int g(int i)
 {
     int x = 5;
-    // CHECK-MESSAGES: :[[@LINE-1]]:9: warning: variable is not initialized via direct list initialization [bsl-var-braced-init]
+    // CHECK-MESSAGES: :[[@LINE-1]]:9: warning: variable 'x' is not initialized via direct list initialization [bsl-var-braced-init]
 
     int y{5};
     int p{};
 
     int z = {5};
-    // CHECK-MESSAGES: :[[@LINE-1]]:9: warning: variable is not initialized via direct list initialization [bsl-var-braced-init]
+    // CHECK-MESSAGES: :[[@LINE-1]]:9: warning: variable 'z' is not initialized via direct list initialization [bsl-var-braced-init]
 
     auto a = y;
+    // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: variable 'a' is not initialized via direct list initialization [bsl-var-braced-init]
+
     auto b{y};
-    // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: variable declared 'auto' is initialized via list initialization [bsl-var-braced-init]
+
     auto c = {y};
-    // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: variable declared 'auto' is initialized via list initialization [bsl-var-braced-init]
+    // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: variable 'c' is not initialized via direct list initialization [bsl-var-braced-init]
 
     char buf[8];
 
@@ -46,17 +48,25 @@ int g(int i)
        c = 42;
     }
 
+    for (char c : buf) {
+        c *= c;
+    }
+
+    for (auto &&c : buf ) {
+        c /= c;
+    }
+
     for (auto i = 0; i < 8; i++) {
+    // CHECK-MESSAGES: :[[@LINE-1]]:15: warning: variable 'i' is not initialized via direct list initialization [bsl-var-braced-init]
         buf[i] += buf[i];
     }
 
     for (auto i{0}; i < 8; i++) {
-    // CHECK-MESSAGES: :[[@LINE-1]]:15: warning: variable declared 'auto' is initialized via list initialization [bsl-var-braced-init]
         buf[i] += buf[i];
     }
 
     for (int i = 0; i < 8; i++) {
-    // CHECK-MESSAGES: :[[@LINE-1]]:14: warning: variable is not initialized via direct list initialization [bsl-var-braced-init]
+    // CHECK-MESSAGES: :[[@LINE-1]]:14: warning: variable 'i' is not initialized via direct list initialization [bsl-var-braced-init]
 
         buf[i] += buf[i];
     }
@@ -66,5 +76,35 @@ int g(int i)
 
 int baz(int i = 0)
 {
-    return i * 2;
+    if (auto j{i}; j > 4)
+        return i * 2;
+    else if (int k = i * 3; k % 5 == 2)
+    // CHECK-MESSAGES: :[[@LINE-1]]:18: warning: variable 'k' is not initialized via direct list initialization [bsl-var-braced-init]
+        return 0;
+
+    int pairs[3][2]{{0,0}, {1,1}, {2,2}};
+    int sum{};
+
+    for (auto&& [first, second] : pairs) {
+        sum += first + second;
+    }
+
+    int buf[4];
+
+    for (int n = sum; auto i : buf) {
+    // CHECK-MESSAGES: :[[@LINE-1]]:14: warning: variable 'n' is not initialized via direct list initialization [bsl-var-braced-init]
+        sum *= n - i * 3;
+    }
+
+    return sum;
+}
+
+struct C {
+   int c;
+};
+
+int make_c()
+{
+    C c;
+    // CHECK-MESSAGES: :[[@LINE-1]]:7: warning: variable 'c' is not initialized via direct list initialization [bsl-var-braced-init]
 }
